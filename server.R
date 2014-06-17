50ac78228ecae6a255d8db8ca9988585421b4259
SandroMatteo <sandromatteo@users.noreply.github.com>
2014-06-17 22:16:09 +0200

    Project shiny for Coursera class

---
+++ server.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+CircleArea <- function(radius){ 3.14*radius*radius}
+CirclePer <- function(radius){ 6.28*radius}
+TriangleArea <- function(vertical,base){base*vertical/2}
+TrianglePer <- function(vertical,base){"not enough information, you need to know the 3 edges"}
+RectangleArea <- function(vertical,base){base*vertical}
+RectanglePer <- function(vertical,base){2*(base+vertical)}
+
+
+shinyServer(
+  function(input,output){
+    output$outArea2<-renderPrint(if (length(input$idBox2) == 0) {} else if (input$idBox2[1] %in% 1){CircleArea(input$idRadius2)} else if (input$idBox2[2] %in% 1){CircleArea(input$idRadius2)} else{})
+    output$outPer2<-renderPrint(if (length(input$idBox2) == 0) {} else if (input$idBox2[1] %in% 2){CirclePer(input$idRadius2)} else if (input$idBox2[2] %in% 2){CirclePer(input$idRadius2)} else {})
+    output$outArea3<-renderPrint(if (length(input$idBox3) == 0) {} else if (input$idBox3[1] %in% 1){TriangleArea(input$idVertical3,input$idBase3)} else if (input$idBox3[2] %in% 1){TriangleArea(input$idVertical3,input$idBase3)} else{})
+    output$outPer3<-renderPrint(if (length(input$idBox3) == 0) {} else if (input$idBox3[1] %in% 2){TrianglePer(input$idVertical3,input$idBase3)} else if (input$idBox3[2] %in% 2){TrianglePer(input$idVertical3,input$idBase3)} else{})
+    output$outArea4<-renderPrint(if (length(input$idBox4) == 0) {} else if (input$idBox4[1] %in% 1){RectangleArea(input$idVertical4,input$idBase4)} else if (input$idBox4[2] %in% 1){RectangleArea(input$idVertical4,input$idBase4)} else{})
+    output$outPer4<-renderPrint(if (length(input$idBox4) == 0) {} else if (input$idBox4[1] %in% 2){RectanglePer(input$idVertical4,input$idBase4)} else if (input$idBox4[2] %in% 2){RectanglePer(input$idVertical4,input$idBase4)} else{})
+  }
+)
---
+++ ui.R
@@ -0,0 +1,65 @@
+library(shiny)
+
+
+
+shinyUI(pageWithSidebar(
+  headerPanel("Basic Geometry"),
+  sidebarPanel(
+    conditionalPanel(condition="input.tabs==1",
+                     h3("Intro"),
+                     helpText("This application calculates the areas or perimeter of some basic geometric figures"),
+                     helpText("click on the tab to select the figure...")
+    ),
+    conditionalPanel(condition="input.tabs==2",
+                     h3("Circle"),
+                     checkboxGroupInput("idBox2","Select what you want calculate",c("Area"= "1",
+                                                           "Circumference"= "2")),
+                     numericInput('idRadius2','radius',0, min=0, max=100, step=1)
+    ),
+    
+    conditionalPanel(condition="input.tabs==3",
+                     h3("Triangle"),
+                     checkboxGroupInput("idBox3","Select what you want calculate",c("Area"= "1",
+                                                           "Perimeter"= "2")),
+                     numericInput('idVertical3','vertical height',0, min=0, max=100, step=1),
+                     numericInput('idBase3','base',0, min=0, max=100, step=1)
+    ),
+    conditionalPanel(condition="input.tabs==4",
+                     h3("Rectangle"),
+                     checkboxGroupInput("idBox4","Select what you want calculate",c("Area"= "1",
+                                                              "Perimeter"= "2")),
+                     numericInput('idVertical4','vertical height',0, min=0, max=100, step=1),
+                     numericInput('idBase4','base',0, min=0, max=100, step=1)
+    )
+  ),
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Intro", 
+               value=1,
+               p("select one of the TABs above !")
+               ), 
+      tabPanel("Circle", 
+               value=2,
+               p("Area"),
+               verbatimTextOutput("outArea2"),
+               p("Circumference"),
+               verbatimTextOutput("outPer2")
+               ),
+      tabPanel("Triangle", 
+               value=3,
+               p("Area"),
+               verbatimTextOutput("outArea3"),
+               p("Perimeter"),
+               verbatimTextOutput("outPer3")
+               ),
+      tabPanel("Rectangle", 
+               value=4,
+               p("Area"),
+               verbatimTextOutput("outArea4"),
+               p("Perimeter"),
+               verbatimTextOutput("outPer4")
+               ),
+      id = "tabs"
+    )
+  )
+))
